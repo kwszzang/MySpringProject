778991829b220e3785e6911e7158adcfa3857ae1
kwszzang <kwsic@naver.com>
2021-03-19 14:14:55 +0900

    초기설정

---
+++ src/main/java/controller/common/MainController.java
@@ -0,0 +1,24 @@
+package controller.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	private ModelAndView mav = null;
+	
+	public MainController() {
+		this.mav  = new ModelAndView();
+	}
+	
+	@GetMapping(value = "main.co")
+	public ModelAndView doGet() {
+		
+		this.mav.setViewName("main");
+		
+		return this.mav;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/common/main.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<% String contextPath = request.getContextPath() ;%>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>메인 화면입니다. 잘됩니까?</h1>
+	
+	<a href = "<%=contextPath%>/main.co"> 메인 화면으로 가는지 확인</a>
+</body>
+</html>
